c375a0d41148bcc6937a2c5c29d2a6a12d8e91d2
Michael Foley <foley.p.michael@gmail.com>
2022-02-19 13:55:03 +0000

    added mapbox code in, map now appearing in main feed.

--- routes/pictureApp.js
+++ routes/pictureApp.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 // GET Main Feed View
 router.get("/main-feed", (req, res) => {
-  res.render("pictureApp/main-feed", { userInSession: req.session.currentUser });
+  res.render("pictureApp/main-feed", { userInSession: req.session.currentUser, accessToken: process.env.MAPBOXGL_ACCESSTOKEN });
 });
 
 // GET User Profile View
